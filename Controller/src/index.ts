ee1fa29f7ae1e93a56e0faf334e6a968a0c32561
OsmarFraust <osmarherfr@gmail.com>
2026-07-17 14:22:58 -0600

    fix: Fix dbConnection Issue

--- Controller/src/index.ts
+++ Controller/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import {TestConection, mySequelize } from './config/dbConection.js';
+import {TestConection, mySequelize } from '../../Db/dbConection.js';
 
 TestConection();
 
