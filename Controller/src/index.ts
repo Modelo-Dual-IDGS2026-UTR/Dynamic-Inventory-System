67e0b8a75bfd770b852e56d305e147540da33edc
OsmarFrausto <osmarherfr@gmail.com>
2026-07-19 17:38:52 -0600

    feat: add test model functioin

--- Controller/src/index.ts
+++ Controller/src/index.ts
@@ -1,37 +1,37 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import {TestConection, mySequelize } from '@dis/db/dbConection.js';
-/*
-import { GenerateJWT , VerifyJWT} from './middleware/jwtUtils.js'; 
-import type { jwtPayloadContent } from './middleware/jwtUtils.js';
-*/
-TestConection();
-/*
-const TestJWT=()=>{
-    const userPayload:jwtPayloadContent = {
-        userId:"6969",
-        role: 1,
-        career_area:"TICS",
-        fullName:"Osmar Macias Curiel"
-    }
+import {TestConection } from '@dis/db/dbConection.js';
+import {User,UserRole} from '../../Model/index.js'
+import { mySequelize } from '@dis/db/dbConection.js';
+const TestSequelize=async ()=>{
+try {
+    await mySequelize.authenticate();
+    console.log("Conexion a DB exitosa\n");
+    const usuarios = await User.findAll();
+    console.log("consulta exitosa\nDATOS:\n");
+    console.log(usuarios);
 
-    let jwt:string =GenerateJWT(userPayload,"2m");
-
-    try{
-        VerifyJWT(jwt);
-        console.log(jwt + "\nToken Validaded")
-    }catch{
-        console.log("DUUUUUUDE YOU FUCKED UP")
-    }
-    
+} catch (error) {
+    console.error('❌ Error en la prueba:', error);
+}finally{
+    await mySequelize.close()
 }
-*/
+
+
+}
+
+TestSequelize();
+
+
 
 const app = express();
 const PORT = 3000;
 
 // Middleware para entender JSON
 app.use(express.json());
+
+
+
 
 // Endpoint GET de prueba
 app.get('/', (req: Request, res: Response) => {
@@ -55,3 +55,32 @@ app.listen(PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+
+//----------------------NOT PRODUCTION CODE------------------------------------------
+        /*
+        import { GenerateJWT , VerifyJWT} from './middleware/jwtUtils.js'; 
+        import type { jwtPayloadContent } from './middleware/jwtUtils.js';
+        */
+        
+        /*
+        const TestJWT=()=>{
+            const userPayload:jwtPayloadContent = {
+                userId:"6969",
+                role: 1,
+                career_area:"TICS",
+                fullName:"Osmar Macias Curiel"
+            }
+        
+            let jwt:string =GenerateJWT(userPayload,"2m");
+        
+            try{
+                VerifyJWT(jwt);
+                console.log(jwt + "\nToken Validaded")
+            }catch{
+                console.log("DUUUUUUDE YOU FUCKED UP")
+            }
+            
+        }
+        */
