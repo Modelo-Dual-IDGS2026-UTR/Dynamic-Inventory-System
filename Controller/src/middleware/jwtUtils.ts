e154b9ad7813770167613569c17297cee2cc819d
OsmarFrausto <osmarherfr@gmail.com>
2026-07-18 18:47:36 -0600

    feat: Add JWT Logic

---
+++ Controller/src/middleware/jwtUtils.ts
@@ -0,0 +1,49 @@
+import jwt from 'jsonwebtoken';
+import type {SignOptions, VerifyOptions} from 'jsonwebtoken';
+import { error } from 'node:console';
+
+export interface jwtPayloadContent{
+    userId:string;
+    fullName:string;
+    role:number;
+    career_area:string;
+};
+
+const JWT_Secret=process.env.JWT_SECRET;
+
+if(!JWT_Secret){
+    throw new Error("\n------FATAL ERROR: No jwt secret defined---------\n");
+}
+
+export const GenerateJWT=(payload:jwtPayloadContent, expiresIn: SignOptions["expiresIn"] = 15000): string =>{
+    const options: SignOptions = {
+        expiresIn,
+        algorithm: 'HS256'
+
+    };
+
+    return jwt.sign(payload,JWT_Secret,options);
+
+}
+
+export const VerifyJWT=(token:string):jwtPayloadContent=>{
+    const options: VerifyOptions = {
+        algorithms:['HS256']
+    }
+
+    try{
+        const decoded = jwt.verify(token,JWT_Secret,options) as jwtPayloadContent;
+        return decoded
+    }catch(error){
+        if (error instanceof jwt.TokenExpiredError){
+            throw new Error("Expired Token");
+        } else if (error instanceof jwt.JsonWebTokenError){
+            throw new Error("Invalid Token");
+        }else{
+            throw new Error("Someting went wrong authenticating this user");
+        }
+        throw new Error ("Auth_error")
+    }
+    
+
+}
