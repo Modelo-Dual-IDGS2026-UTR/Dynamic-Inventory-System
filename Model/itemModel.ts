373672f2f6835dc71075bda345e07c455dfc5234
OsmarFrausto <osmarherfr@gmail.com>
2026-07-22 16:27:48 -0600

    feat: add item model

---
+++ Model/itemModel.ts
@@ -0,0 +1,55 @@
+import {mySequelize} from '@dis/db/dbConection'
+import { DataTypes } from 'sequelize'
+
+enum Categories{
+    NOCATEGORY="No Category"
+}
+
+export const Item = mySequelize.define("Item",{
+    itemId:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    itemName:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    itemDescription:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    codeBar:{type:DataTypes.STRING},
+    category:{
+        type:DataTypes.ENUM(...Object.values(Categories)),
+        defaultValue:Categories.NOCATEGORY,
+
+    },
+    fk_user_responsible:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+            model:"User",
+            key:"userId"
+        },
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE'
+
+    },
+    fk_place:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+            model:"Place",
+            key:"placeId"
+        },
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE'
+
+
+    },
+    createdAt:{type:DataTypes.DATE},
+    updatedAt:{type:DataTypes.DATE}
+},{
+    tableName:"Item"
+})
