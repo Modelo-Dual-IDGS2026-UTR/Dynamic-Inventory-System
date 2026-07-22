6e01d737c3eda6cc848d85e5e64ddf48ecac6fa9
ChaosKingM <emmanuelelegantini@gmail.com>
2026-07-21 22:01:51 -0600

    fix: fixed docker not detecting client id from google

--- View/frontend/src/App.jsx
+++ View/frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
 
 function App() {
 
-  // Función para recibir la respuesta de Google tras iniciar sesión
   const handleSuccess = (credentialResponse) => {
     console.log('Token de Google:', credentialResponse.credential)
-    // Aquí mandas credentialResponse.credential a tu Backend (Controller) para verificarlo
+    // Aquí mandas credentialResponse.credential a tu backend para verificarlo
   }
 
   const handleError = () => {
@@ -14,21 +12,18 @@ function App() {
   }
 
   return (
-    // Envuelves tu app o componente con el Provider de Google
-    <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
         <h2>Iniciar Sesión</h2>
         
-        {/* Este componente renderiza el botón oficial automáticamente */}
         <GoogleLogin
           onSuccess={handleSuccess}
           onError={handleError}
-          useOneTap // Opcional: Muestra una pequeña ventana emergente arriba a la derecha
+          useOneTap
         />
       </div>
     </GoogleOAuthProvider>
   )
-
 }
 
 export default App
