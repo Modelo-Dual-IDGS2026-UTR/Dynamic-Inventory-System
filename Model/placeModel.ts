79bf87c85f24084ce6ba1afe31f1d9ee7dbd657e
OsmarFrausto <osmarherfr@gmail.com>
2026-07-22 16:34:42 -0600

    feat: add placeModel

---
+++ Model/placeModel.ts
@@ -0,0 +1,35 @@
+import { mySequelize } from "@dis/db/dbConection";
+import {DataTypes} from 'sequelize'
+
+export const Place=mySequelize.define("Place",{
+    placeId:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        allowNull:false
+    },
+    placeName:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    placeDescription:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    class:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    ip_range:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    placeLocation:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    createdAt:{type:DataTypes.DATE},
+    updatedAt:{type:DataTypes.DATE}
+
+},{
+    tableName:"Place"
+})
