ba2101b93c69b2f22fe14eb18be32d38207193fd
OsmarFraust <osmarherfr@gmail.com>
2026-07-30 15:22:49 -0600

    feat: Add google signin logic

--- Controller/src/middleware/googleToken.ts
+++ Controller/src/middleware/googleToken.ts
@@ -1,4 +1,39 @@
-import express from 'express'
+import express, { type NextFunction, type Request, type Response } from 'express'
 import { OAuth2Client } from 'google-auth-library'
 
-const GoogleClient = process.env.VITE_GOOGLE_CLIENT_ID
+const GoogleClientID = process.env.VITE_GOOGLE_CLIENT_ID||""
+const client = new OAuth2Client(GoogleClientID)
+
+export const VerifyGoogleToken=async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const rawToken=req.headers.authorization
+        if(!rawToken||!rawToken.startsWith('Bearer ')){
+            return res.status(401).json({
+                message:"No token recived: ACCESS DENIED .|." 
+            })
+        }
+        const uncodedToken=rawToken.split(' ')[1]
+        if(!uncodedToken){
+            return res.status(401).json({
+                message:"No token recived: ACCESS DENIED .|." 
+            })
+        }
+        const ticket= await client.verifyIdToken({
+            idToken:uncodedToken,
+            audience: GoogleClientID
+        })
+        const payload=ticket.getPayload()||{}
+        
+        res.locals.user=payload
+        next()
+
+
+        
+    } catch (error) {
+         res.status(500).json({
+            message:"Internal server error, not your fault :D",
+            error:error})
+    }
+    
+
+}
--- Controller/src/routes/userRoutes.ts
+++ Controller/src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { VerifyJWT } from '../middleware/jwtUtils.js'
 import { userController } from '../controllers/userController.js'
-
+import { VerifyGoogleToken } from '../middleware/googleToken.js'
 
 export const userRouter= express.Router()
 
@@ -10,7 +10,7 @@ export const userRouter= express.Router()
 userRouter.post('/create-user',VerifyJWT(1),userController.CreateUser)
 
 
-userRouter.get('/login',userController.LogUser)
+userRouter.get('/login',VerifyGoogleToken,userController.LogUser)
 //Add endpoint to docs
 
 userRouter.get('/findUser/:userId',VerifyJWT(),userController.SearchUserById)
