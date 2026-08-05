db3de54a729d31d4652acc0ab6209a9078f1e220
ChaosKingM <emmanuelelegantini@gmail.com>
2026-08-04 22:04:00 -0600

    fix(protected_routes): fix error where the authentication will load infinitely

--- View/frontend/src/App.tsx
+++ View/frontend/src/App.tsx
@@ -2,15 +2,18 @@ import { JSX} from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './routes/AppRouter';
+import { AuthProvider } from './utils/auth';
 
 export default function App(): JSX.Element {
   const clientId: string = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <BrowserRouter>
-          <AppRouter />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
+      </AuthProvider>
     </GoogleOAuthProvider>
   );
 }
