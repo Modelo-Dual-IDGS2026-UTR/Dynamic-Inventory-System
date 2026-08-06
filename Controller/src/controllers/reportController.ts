d52744ccfc7b766c5570d2c9f4aa55eedc4d8a91
ChaosKingM <emmanuelelegantini@gmail.com>
2026-08-06 13:20:23 -0600

    feat(reportController): add read-report, all reports and user-reports endpoints

--- Controller/src/controllers/reportController.ts
+++ Controller/src/controllers/reportController.ts
@@ -24,7 +24,7 @@ const CreateReport = async (req:Request, res:Response) => {
         }
 
 
-        const newReport = await Report.create({
+        await Report.create({
             reportName,
             reportDescription,
             reportStatus,
@@ -49,7 +49,7 @@ const CreateReport = async (req:Request, res:Response) => {
 
 const SearchReportById = (req:Request, res:Response) => {
     try {
-        const id = req.params.reportId
+        const id = req.params.id
 
         if(!id){
             return res.status(400).json({
@@ -101,9 +101,6 @@ async function ShowReport(id:number,res:Response){
             })
         }
         const report = foundReport.toJSON()
-        const fullName=
-        report.related_user.firstName+
-        report.related_user.lastName
         
         report.fk_place={
             placeId: report.related_place.placeId,
@@ -112,13 +109,13 @@ async function ShowReport(id:number,res:Response){
         
         report.fk_user_responsible={
             userId:report.related_user.userId,
-            
-            userName:fullName
+            firstname:report.related_user.firstName,
+            userName:report.related_user.lastName
         }
 
         report.fk_item={
-            itemId:report.related_item.itemId,
-            itemName:report.related_item.itemName
+            reportId:report.related_item.itemId,
+            reportName:report.related_item.itemName
         }
 
         delete report.related_place
@@ -135,7 +132,224 @@ async function ShowReport(id:number,res:Response){
 }
 }
 
+const SearchReports = async (req:Request,res:Response)=>{
+    try {
+        const body=req.body||{}
+        const {reportId,
+            reportName,
+            reportDescription,
+            reportStatus,
+            reportPriority,
+            dueDate,
+            fk_user,
+            fk_item,
+            fk_place,
+            sortBy='reportId',
+            order='ASC'
+            }=body
+        const searchOptions=await FilterOptions({
+            reportId,
+            reportName,
+            reportDescription,
+            reportStatus,
+            reportPriority,
+            dueDate,
+            fk_user,
+            fk_item,
+            fk_place
+        })
+            const foundReports= await Report.findAll({
+                    where:searchOptions,
+                    include:[{
+                    model: Place,
+                    as: 'related_place',
+                    attributes:['placeId','placeName']
+
+                },
+                {
+                    model: User,
+                    as:'related_user',
+                    attributes:['userId','firstName', 'lastName']
+                },
+                {
+                    model: Item,
+                    as: 'related_item',
+                    attributes: ['itemId', 'itemName',]
+                }
+                    ],
+                    order:[[sortBy,order.toUpperCase()]]
+            })
+
+            const formatedReports=foundReports.map((reportInstance)=>{
+                const report=reportInstance.toJSON()
+                const formated={
+                    ...report,
+                    fk_place:report.related_place?
+                            {
+                                id:report.related_place.placeId,
+                                name:report.related_place.placeName
+                            }:report.related_place.placeId,
+                    fk_user:report.related_user?
+                            {
+                                id:report.related_user.userId,
+                                firstName:report.related_user.firstName,
+                                lastName:report.related_user.lastName
+                            }:report.related_user.userId,
+                    fk_item:report.related_item?
+                            {
+                                id:report.related_item.itemId,
+                                itemName:report.related_item.itemName
+                            }:report.related_item.itemId,
+                    
+                    
+                    
+                }
+                delete formated.related_place
+                delete formated.related_user
+                delete formated.related_item
+
+                return formated
+
+
+            })
+            return res.status(200).json(formatedReports)
+            
+        } catch (error) {
+            res.status(500).json({
+            message:"Internal server error, not your fault :D",
+            error:error
+    })
+    }    
+}
+
+const SearchReportsCreatedUser = async (req:Request,res:Response)=>{
+    try {
+        const userId = req.params.userId;
+
+        if (!userId) {
+            return res.status(400).json({
+                message: "User ID is required"
+            });
+        }
+
+        const convertedUserId = Number(userId);
+
+        if (isNaN(convertedUserId) || !Number.isInteger(convertedUserId) || convertedUserId <= 0) {
+            return res.status(400).json({
+                message: "Invalid User ID"
+            });
+        }
+
+        const body=req.body||{}
+
+        const {
+            reportId,
+            reportName,
+            reportDescription,
+            reportStatus,
+            reportPriority,
+            dueDate,
+            fk_item,
+            fk_place,
+            sortBy='reportId',
+            order='ASC'
+        }=body
+
+        const searchOptions=await FilterOptions({
+            reportId,
+            reportName,
+            reportDescription,
+            reportStatus,
+            reportPriority,
+            dueDate,
+            fk_user: convertedUserId,
+            fk_item,
+            fk_place
+        })
+            
+        const foundReports= await Report.findAll({
+                where:searchOptions,
+                include:[{
+                model: Place,
+                as: 'related_place',
+                attributes:['placeId','placeName']
+
+            },
+            {
+                model: User,
+                as:'related_user',
+                attributes:['userId','firstName', 'lastName']
+            },
+            {
+                model: Item,
+                as: 'related_item',
+                attributes: ['itemId', 'itemName',]
+            }
+                ],
+                order:[[sortBy,order.toUpperCase()]]
+        })
+
+        const formatedReports=foundReports.map((reportInstance)=>{
+            const report=reportInstance.toJSON()
+            const formated={
+                ...report,
+                fk_place:report.related_place?
+                        {
+                            id:report.related_place.placeId,
+                            name:report.related_place.placeName
+                        }:report.related_place.placeId,
+                fk_user:report.related_user?
+                        {
+                            id:report.related_user.userId,
+                            firstName:report.related_user.firstName,
+                            lastName:report.related_user.lastName
+                        }:report.related_user.userId,
+                fk_item:report.related_item?
+                        {
+                            id:report.related_item.itemId,
+                            itemName:report.related_item.itemName
+                        }:report.related_item.itemId,
+                
+                
+                
+            }
+            delete formated.related_place
+            delete formated.related_user
+            delete formated.related_item
+
+            return formated
+        })
+
+        
+        return res.status(200).json(formatedReports);
+    } catch (error) {
+        res.status(500).json({
+        message:"Internal server error, not your fault :D",
+        error:error
+    })
+}}
+
+
+function FilterOptions<T extends object=Record<string,unknown>>(
+    filter:Record<string,unknown>
+):WhereOptions<T>{
+    const whereClause:WhereOptions<T>={}
+
+    Object.entries(filter).forEach(([key,value])=>{
+        if(value!==undefined&&value!==null&&value!==""){
+            (whereClause as Record<string, unknown>)[key] = value;
+        }
+    })
+    return whereClause     
+}
+
+
+
+
+
 export const reportController={
     CreateReport,
-    SearchReportById
+    SearchReportById,
+    SearchReports,
+    SearchReportsCreatedUser
 }
--- Controller/src/routes/reportRoutes.ts
+++ Controller/src/routes/reportRoutes.ts
@@ -4,4 +4,8 @@ export const reportRouter=express.Router();
 
 reportRouter.post('/new-report',reportController.CreateReport);
 
-reportRouter.get('/read-report/:reportId', reportController.SearchReportById)
+reportRouter.get('/read-report/:id', reportController.SearchReportById);
+
+reportRouter.get('/all-reports',reportController.SearchReports);
+
+reportRouter.get('/user-reports/:userId',reportController.SearchReportsCreatedUser);
